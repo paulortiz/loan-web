59975de82ccda789858f129bc8bde2f81eadecf0
paulortiz <pjortiz07@gmail.com>
2018-08-12 16:13:25 +0800

    Updated css properties of Slider and App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Calculate your desired loan</h1>
+        <div class="wrapper">
+            <Slider 
+              title="How much would you like to loan?" 
+              :min="25000"               
+              :max="100000"
+              minDisplay="S$ 25,000"
+              maxDisplay="S$ 100,000"/>
+
+            <Slider
+              title="What is your preferred tenor?" 
+              :min="1"               
+              :max="24"
+              minDisplay="1 month"
+              maxDisplay="24 months"/>
+            
+            <button name="calculate"> Calculate </button>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Slider from './components/slider/Slider.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        Slider
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./Style.css"></style>
---
+++ src/components/slider/Slider.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <h3>{{ title }}</h3>
+        <div class="input-wrapper">
+            <input type="text" class="amount-field" :value="sliderValue">
+            <div class="range-slider-container">
+                <input v-model="sliderValue" type="range" :min="min" :max="max" class="slider" @change="sliderChange" />
+                <div class="amount-range-container">
+                    <div id="min">{{ minDisplay }}</div>
+                    <div id="max">{{ maxDisplay }}</div>
+                </div>
+            </div> 
+        </div> 
+    </div>    
+</template>
+
+<script>
+    export default {
+        name: "Slider",
+        props: {
+            title: String,
+            name: String,
+            min: Number,
+            max: Number,
+            minDisplay: String,
+            maxDisplay: String
+        },
+        data: function() {
+            return {
+                sliderValue: 0
+            }
+        },
+        methods: {
+            sliderChange() {                
+                this.$emit("sliderChange", this.sliderValue)
+            }
+        }
+    }
+</script>
+
+<style src="./Slider.css"></style>
