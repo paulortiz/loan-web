570871e46887f7ce5d3e491feecd65df706602aa
paulortiz <pjortiz07@gmail.com>
2018-08-13 23:47:12 +0800

    Added validations in front-end

--- src/App.vue
+++ src/App.vue
@@ -21,15 +21,17 @@
             <button name="calculate" v-on:click="getRepayment"> Calculate </button>       
         </div>          
         <div class="breaker">&nbsp;</div>
-
-        <h2>Computed Repayment</h2>
-        <div class="wrapper">
-            <div class="computed-repayment">
-                <ResultItem title="Monthly" value="S$4,500" />
-                <ResultItem title="Interest Rate" value="1.2 %" />
-                <ResultItem title="Interest Amount" value="S$101, 200" />
-                <ResultItem title="Tenor" value="1 year, 8 months" />                
-            </div>            
+        <div>
+            <h2>Computed Repayment</h2>
+            <div class="wrapper">
+                <div class="computed-repayment">
+                    <ResultItem title="Original Principal" v-bind:value="repayment.originalPrincipal | amount" />                    
+                    <ResultItem title="Monthly Installment" v-bind:value="repayment.monthly | amount" />
+                    <ResultItem title="Interest Rate" v-bind:value="repayment.interest | percent" />
+                    <ResultItem title="Interest Amount" v-bind:value="repayment.interestAmount | amount" />
+                    <ResultItem title="Ending Principal" v-bind:value="repayment.endingPrincipal | amount" />
+                </div>            
+            </div>
         </div>
     </div>
 </template>
@@ -38,6 +40,7 @@
 //imports
 import Slider from './components/slider/Slider.vue'
 import ResultItem from './components/ResultItem.vue'
+import axios from 'axios'
 
 export default {
     name: 'app',
@@ -45,11 +48,48 @@ export default {
         Slider,
         ResultItem
     },
+    data: function() {
+        return {
+            repayment: {}
+        }
+    },
     methods : {
         getRepayment: function() {
             let loanAmount = this.$refs.amountSlider.sliderValue;
             let tenor = this.$refs.tenorSlider.sliderValue
-            console.log(`Loan amount ${loanAmount}, tenor: ${tenor}`)
+            const baseURL = "http://localhost:3000"
+            const headers = { 'amount': loanAmount, 'tenure': tenor }
+            
+            if (loanAmount < 25000 || loanAmount > 100000) {
+                alert("Please choose amount between 25,000 to 100,000")
+                return
+            }
+
+            if (tenor < 1 || tenor > 24) {
+                alert("Please choose tenor between 1 to 24")
+                return
+            }
+            
+            axios.get(`${baseURL}/repayment`, { headers }).then(response => {
+                console.log("===>>>", response.data)
+                let data = response.data;
+                let error = data.error;
+                this.repayment = data.body;
+            }).catch(e => {
+                console.error(`Failed to get response error : ${e}`)
+            })
+        }
+    },
+    filters: {
+        amount : function(value) {
+            if (!value) return ''
+            value = parseFloat(value).toFixed(2)
+            return Number(value).toLocaleString('en');
+        },
+
+        percent : function(value) {
+            if (!value) return ''
+            return `${value}%`
         }
     }
 }
--- src/components/slider/Slider.vue
+++ src/components/slider/Slider.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <h3>{{ title }}</h3>
         <div class="input-wrapper">
-            <input type="text" class="amount-field" :value="sliderValue">
+            <input type="text" v-bind:class="{ valid : valid, invalid : invalid }" class="amount-field" :value="sliderValue | amount" v-on:change="inputChange">
             <div class="range-slider-container">
                 <input v-model="sliderValue" type="range" :min="min" :max="max" class="slider" @change="sliderChange" />
                 <div class="amount-range-container">
@@ -27,12 +27,44 @@ export default {
     },
     data: function() {
         return {
-            sliderValue: 0
+            sliderValue: this.$props.min,
+            valid: true,
+            invalid: false
         }
     },
     methods: {
         sliderChange() {                
             this.$emit("sliderChange", this.sliderValue)
+            let value = this.sliderValue
+            let max = this.$props.max
+            let min = this.$props.min
+            if (value < min || value > max) {
+                this.$data.valid = false
+                this.$data.invalid = true
+            } else {
+                this.$data.valid = true
+                this.$data.invalid = false
+            }
+        },
+        inputChange($event) {
+            let value = $event.target.value
+            let max = this.$props.max
+            let min = this.$props.min
+            if (value < min || value > max) {
+                this.$data.valid = false
+                this.$data.invalid = true
+            } else {
+                this.$data.valid = true
+                this.$data.invalid = false
+            }
+            this.sliderValue = $event.target.value;
+        }
+    },
+    filters: {
+        amount : function(value) {
+            if (!value) return value
+            value = parseFloat(value).toFixed(2)
+            return Number(value).toLocaleString('en')
         }
     }
 }
